7b712af84cd1b3e8262eefeb7a568fc6ec63e6b4
Henry Darnell <hjdarnel@uark.edu>
2016-02-21 17:39:57 -0600

    Finished assignment

--- Client.java
+++ Client.java
@@ -10,22 +10,23 @@ import javax.imageio.ImageIO;
 public class Client {
     Image newimg;
     static BufferedImage bimg;
-    byte[] bytes;
 
     public static void main(String [] args)
     {
-        String serverName = "localhost";
+        String serverName = "192.168.1.37"; //EDIT THIS TO NETWORKED SERVER COMPUTER IP
         int port = 6066;
         String[] filenames = {"test1.jpg", "test2.jpg", "test3.jpg", "test4.jpg", "test5.jpg"};
 
         for (String x : filenames){
           try
           {
+              //open socket connection
               Socket client = new Socket(serverName, port);
+              //write image to BufferedImage instance
               bimg = ImageIO.read(new File(x));
-
+              //send image to socket
               ImageIO.write(bimg,"JPG",client.getOutputStream());
-
+              //close socket after sending image, so server knows that the image is fully transmitted
               client.close();
           } catch(IOException e) {
               e.printStackTrace();
--- Server.java
+++ Server.java
@@ -13,22 +13,29 @@ import javax.swing.ImageIcon;
 
 public class Server {
   public static void main(String[] args) {
-    int counter = 1;
+
+    int counter = 1; //for output filenames
+
     try{
       ServerSocket serverSocket = new ServerSocket(6066);
       while(true){
+        //open connetion to receive data
         Socket client = serverSocket.accept();
+        //read all of the Image from the inputstream of the socket into a BufferedImage
+        BufferedImage img = ImageIO.read(ImageIO.createImageInputStream(client.getInputStream()));
 
-        BufferedImage img=ImageIO.read(ImageIO.createImageInputStream(client.getInputStream()));
+        //display the buffered image in a JFrame
         JFrame frame = new JFrame();
         frame.getContentPane().add(new JLabel(new ImageIcon(img)));
         frame.pack();
         frame.setVisible(true);
 
-        File outputfile = new File("image"+counter+".jpg");
+        //for completeness, also save the image to the immediate directory. Could probably implement caching somehow.
+        File outputfile = new File("transmittedimage"+counter+".jpg");
         ImageIO.write(img, "jpg", outputfile);
         counter++;
 
+        //close the socket because there is no more data to receive
         client.close();
       }
 
